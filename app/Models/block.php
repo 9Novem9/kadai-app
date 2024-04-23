2ca25c51b8f120c498dc0ed8ea7d47e1430d5cc4
302A-13\cre <dfsfes>
2024-04-23 19:25:39 +0900

    モデルをいじる

---
+++ app/Models/block.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+
+class Follow extends Model
+{
+    use HasFactory;
+
+ 
+    public function block()
+    {
+    
+    }
+
+   
+    public function as()
+    {
+    
+    }
+}
